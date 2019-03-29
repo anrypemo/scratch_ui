7a2b09fc8356edee6dec9c84937ae42c6b127a65
pemo0717 <petro.moiseienko@netcracker.com>
2019-03-29 19:52:22 +0200

    loader type added

--- src/app/components/shared-components/ui-components/loader/loader.component.ts
+++ src/app/components/shared-components/ui-components/loader/loader.component.ts
@@ -13,9 +13,24 @@ export class UiLoaderComponent implements OnInit {
   @Input()
   public visible: boolean = false;
 
+  @Input()
+  public type: string = "rose";
+
+  public loaderImageUrl="";
+
   constructor() { }
 
   ngOnInit() {
+    switch(this.type){
+      case "rose": {
+        this.loaderImageUrl = "/assets/images/loading-rose.png";
+        break;
+      }
+      case "plane": {
+        this.loaderImageUrl = "/assets/images/loading-plane.png";
+        break;
+      }
+    }
   }
 
 }
